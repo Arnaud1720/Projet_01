5bfda1b7b538f8699afefe2cdafa0aa443a6ee13
DERISBOURG <derisbourgarnaud@gmail.com>
2024-12-12 19:08:30 +0100

    ajout de la création auto d'un utilisateur adm

--- src/main/java/com/arnaud/ia/api/ApiApplication.java
+++ src/main/java/com/arnaud/ia/api/ApiApplication.java
@@ -1,12 +1,43 @@
 package com.arnaud.ia.api;
 
+import com.arnaud.ia.api.entity.Roles;
+import com.arnaud.ia.api.entity.Utilisateurs;
+import com.arnaud.ia.api.repository.UtilisateurRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 @SpringBootApplication(scanBasePackages = "com.arnaud.ia.api")
 public class ApiApplication {
+
+	@Bean
+	public CommandLineRunner createAdminUser(UtilisateurRepository utilisateurRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			String email = "admin@example.com";
+			// Vérifier si l'utilisateur n'existe pas déjà
+			if (!utilisateurRepository.existsByEmail(email)) {
+				Utilisateurs admin = new Utilisateurs();
+				admin.setNom("Admin");
+				admin.setPrenom("Super");
+				admin.setEmail(email);
+				// Mot de passe en clair, à hacher :
+				admin.setPassword(passwordEncoder.encode("monmotdepasse"));
+				admin.setTelephone("0123456789");
+				admin.setRole(Roles.Administrateur);
+
+				utilisateurRepository.save(admin);
+				System.out.println("Utilisateur administrateur créé au démarrage : " + email);
+			} else {
+				System.out.println("L'utilisateur admin existe déjà, aucune action effectuée.");
+			}
+		};
+	}
+
 
 	public static void main(String[] args) {
 		// Demander à l'utilisateur de saisir le chemin
